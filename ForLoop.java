9213bbaabd81548a33fbbbc911570a814e9d6202
shubhamhr07 <shubhamhr07@github.com>
2020-09-30 03:31:15 +0530

    3.31

---
+++ ForLoop.java
@@ -0,0 +1,18 @@
+class ForeLoop{
+    public static void main(String[] args)
+    {
+        int count = 0;
+        int sum = 0;
+        for(int i = 1;i<=1000;i++){
+            if (i%3==0 && i%5==0){
+                sum+=i;
+                System.out.println("Found Number "+i);
+                count+=1;
+                if (count==5){
+                    break;
+                }
+            }
+        }
+        System.out.println("sum"+sum);
+    }
+}
